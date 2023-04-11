bd9def07c65ba3c61e5e5e31500235c20038a183
AAnn1912 <phamthienan862@gmail.com>
2023-04-11 22:05:06 +0700

    done

--- src/Component/Item.jsx
+++ src/Component/Item.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import './style/style.css'
 
 export default class Item extends Component {
     
   render() {
     let iCon = this.props.iCon
     let conTent = this.props.conTent
+    let title = this.props.title
     console.log(iCon)
     return (
-      <div>
-        <p>{iCon}</p>
+      <div className='item'>
+        <i className={iCon}></i>
+        <h3>{title}</h3>
         <p>{conTent}</p>
       </div>
     )
--- src/Component/Items.jsx
+++ src/Component/Items.jsx
@@ -9,7 +9,35 @@ export default class Items extends Component {
         <section className="Items">
             <div className="container">
                 <div className="list_item">
-                   <Item className="item" iCon="<i className='bx bx-collection'></i>" conTent="<h3>Fresh new layout</h3><p>With Bootstrap 5, we've created a fresh new layout for this template!</p>"/>
+                   <Item 
+                   iCon="bx bx-collection" 
+                   title="Fresh new layout"
+                   conTent="With Bootstrap 5, we've created a fresh new layout for this template"/>
+                   
+                   <Item 
+                   iCon="bx bx-cloud-download" 
+                   title="Free to download"
+                   conTent="As always, Start Bootstrap has a powerful collectin of free templates."/>
+                   
+                   <Item 
+                   iCon="bx bx-credit-card-front" 
+                   title="Jumbotron hero heade"
+                   conTent="The heroic part of this template is the jumbotron hero header!"/>
+                   
+                   <Item 
+                   iCon="bx bxl-bootstrap" 
+                   title="Feature boxes"
+                   conTent="We've created some custom feature boxes using Bootstrap icons!"/>
+                   
+                   <Item 
+                   iCon="bx bx-code" 
+                   title="Simple clean code"
+                   conTent="We keep our dependencies up to date and squash bugs as they come!"/>
+                   
+                   <Item 
+                   iCon="bx bxs-badge-check" 
+                   title="bx bxs-badge-check"
+                   conTent="Start Bootstrap has been the leader in free Bootstrap templates since 2013!"/>
                 </div>
             </div>
         </section>
